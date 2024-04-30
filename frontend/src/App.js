f748155d0edc7774c7488c692dccdb6c65e31fde
JIHO <x8608666@gmail.com>
2024-04-30 19:41:38 +0900

    trading view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,21 @@
-import React, { useEffect, useRef } from 'react';
-import './App.css';
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
 
-const TradingViewWidget = ({ symbol }) => {
-  const widgetRef = useRef(null);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://s3.tradingview.com/tv.js';
-    script.async = true;
-    script.onload = () => {
-      new window.TradingView.widget({
-        "container_id": widgetRef.current.id,
-        "symbol": symbol,
-        "interval": "D",
-        "timezone": "Etc/UTC",
-        "theme": "light",
-        "style": "1",
-        "locale": "kr",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "allow_symbol_change": true,
-        "details": true,
-      });
-    };
-    widgetRef.current.appendChild(script);
-  }, [symbol]); // 종목 코드가 변경될 때마다 위젯을 다시 로드합니다.
-
-  return <div ref={widgetRef} id="tradingview-widget-container" />;
+const Chart = () => {
+  return (
+    <AdvancedRealTimeChart
+      theme="dark"
+      width={850}
+      height={600}
+      symbol="BINANCE:BTCUSDT"
+      interval="D"
+      timezone="Asia/Seoul"
+      style="1"
+      locale="kr"
+      toolbar_bg="#f1f3f6"
+      allow_symbol_change={false}
+      container_id="tradingview_dcf24"
+    ></AdvancedRealTimeChart>
+  );
 };
 
-export default TradingViewWidget;
+export default Chart;
